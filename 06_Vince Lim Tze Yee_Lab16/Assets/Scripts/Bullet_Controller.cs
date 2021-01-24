d1e48bbfe6972c067ec1be6d80677676292734a4
YukimiSnow <happyvince999@gmail.com>
2021-01-25 01:18:24 +0800

    Yes
    Yes Yes

---
+++ 06_Vince Lim Tze Yee_Lab16/Assets/Scripts/Bullet_Controller.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet_Controller : MonoBehaviour
+{
+    public float speed;
+
+    float zLimit = 10f;
+    float xLimit = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, 3);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.forward * speed * Time.deltaTime;
+
+        if(transform.position.x > xLimit)
+        {
+            Destroy(gameObject);
+        }
+
+        if (transform.position.x < -xLimit)
+        {
+            Destroy(gameObject);
+        }
+
+        if (transform.position.z > zLimit)
+        {
+            Destroy(gameObject);
+        }
+
+        if (transform.position.z < -zLimit)
+        {
+            Destroy(gameObject);
+        }
+
+
+    }
+}
